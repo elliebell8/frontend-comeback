dd5f47fa76796ff9eedaad0f0bcad1775bfb3a9c
Paige Ballard <ballarddesignstudio@gmail.com>
2026-09-18 12:08:48 -0500

    start JS reactivation

---
+++ day-01/script.js
@@ -0,0 +1,14 @@
+const menuItems = [
+    "burger",
+    "fries",
+    "soda",
+    "cake"
+];
+console.log(menuItems);
+
+const burger = {
+    name: "burger",
+    price: 10.99,   
+    category: "main course",
+}
+console.log(burger);
